b2aa64d4b85e57f5778fe6b32835eae6c570e62f
MontyPan <monty.pan@dontcareabout.us>
2015-11-06 12:03:19 +0800

    以不會導致 crash 的簡單方式重現 QuotaExceededError

--- src/main/java/com/dtc/test/client/TestStorage.java
+++ src/main/java/com/dtc/test/client/TestStorage.java
@@ -1,5 +1,6 @@
 package com.dtc.test.client;
 
+import com.google.gwt.core.client.JavaScriptException;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.storage.client.Storage;
 
@@ -7,16 +8,21 @@ public class TestStorage {
 	Storage storage = Storage.getLocalStorageIfSupported();
 
 	public void test() {
-		StringBuilder stringBuilder = Generate.generateString();
-		while (true) {
-			try {
-				storage.setItem("1", stringBuilder.append("1").toString());
-			} catch (Exception e) {
-				if (e.getMessage().contains("QuotaExceededError")) {
-					GWT.log(String.valueOf(stringBuilder.toString().getBytes().length));
-					break;
-				}
+		String data = Generate.byMB(5);
+		
+		try {
+			storage.setItem("1", data);
+		} catch (JavaScriptException e) {
+			if ("QuotaExceededError".equals(e.getName())) {
+				GWT.log("超出大小：" + data.getBytes().length);
+				return;
 			}
+			
+			//其他未知的 JAvaScriptException
+			GWT.log("[" + e.getName() + "] : " + e.getMessage());
+		} catch (Exception e) {
+			//不知道有沒有可能炸到這邊來，預防萬一還是 catch 一下
+			GWT.log("[" + e.getClass().getName() + "] : " + e.getMessage());
 		}
 	}
 
@@ -40,6 +46,7 @@ public class TestStorage {
 		build(5119, 1002, over, true);
 	}
 	
+	@Deprecated
 	private void build(int i1, int i2, int over, boolean keyOrData) {
 		StringBuilder stringBuilder = new StringBuilder();
 		for (int i = 0; i < i1; i++) {
